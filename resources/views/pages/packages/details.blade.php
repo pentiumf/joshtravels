eb040ac72b1afcdf2d0a85015d72405cebef0f38
Nick Sw <swaggarnick@gmail.com>
2017-08-31 00:19:47 +0000

    Mailer Activated 14

--- resources/views/pages/packages/details.blade.php
+++ resources/views/pages/packages/details.blade.php
@@ -9,10 +9,10 @@
         <div class="our-pacakge-details-bg-cap-left">
           <h2>{{$package->name}}</h2>
           <div class="pack-wrap clearfix">
-            <div class="pack-left">
+            <!-- <div class="pack-left">
               <span><i class="fa fa-tag"></i></span>
               <p>$100</p>
-            </div>
+            </div> -->
             <div id="packLeftLast" class="pack-left">
               <span><i class="fa fa-globe"></i></span>
               <p>{{$package->destination}}</p>
@@ -47,10 +47,10 @@
           <p>{{$package->end_date}}</p>
         </div>
         <div class="our-pacakge-details-content">
-          <h3>Package Includes</h3>
+          <!-- <h3>Package Includes</h3>
           <p>Visa fee</p>
           <p>Flight Tickets</p>
-          <p>Secure Flight</p>
+          <p>Secure Flight</p> -->
         </div>
       </div>
       <div class="our-pacakge-details-right">
@@ -63,10 +63,10 @@
             <i class="fa fa-clock-o"></i>
             <span>{{$package->duration}}</span>
           </p>
-          <p>
+          <!-- <p>
             <i class="fa fa-tag"></i>
             <span>{{$package->price}}</span>
-          </p>
+          </p> -->
           <p>
             <i class="fa fa-circle"></i>
             <span>{{$package->category->name}}</span>
@@ -79,47 +79,13 @@
         </div>
       </div>
       <div class="our-pacakge-details-bottom">
-        <div class="our-pacakge-details-recommend">
+        <!-- <div class="our-pacakge-details-recommend">
           <div class="recommend-pack clearfix">
             <h2>Recommended Tours</h2>
 
-            @if($recommendedPackages)
-            @foreach($recommendedPackages as $recommendedPackage)
-            <div class="rec-package-item">
-                <img class="img-circle" src="{{$recommendedPackage->photo ? $recommendedPackage->photo : 'https://gitlab.kitware.com/assets/no_group_avatar-4a9d347a20d783caee8aaed4a37a65930cb8db965f61f3b72a2e954a0eaeb8ba.png'}}" alt="">
-                <div class="package-caption clearfix">
-                  <div class="package-caption-left">
-                    <div class="package-caption-name">
-                      <h2>{{$recommendedPackage->name}}</h2>
-                    </div>
-                    <div class="package-caption-rating">
-                      <ul>
-                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="package-caption-right">
-                    <div class="package-caption-save">
-                      <span><i class="fa fa-bookmark-o" aria-hidden="true"></i></span>
-                    </div>
-                    <div class="package-caption-price">
-                      <span>{{$recommendedPackage->price}}</span>
-                    </div>
-                  </div>
-                </div>
-                <div class="package-day-night">
-                  <span>{{$recommendedPackage->duration}}</span>
-                </div>
-                <a href="{{route('packages.details', $recommendedPackage->id)}}"></a>
-            </div>
-            @endforeach
-            @endif
+
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
 
